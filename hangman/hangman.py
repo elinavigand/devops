7f3e5ee2681647131237183e3406f378492d3b09
Elina Vigand <elinavigand@gmail.com>
2024-11-14 09:34:41 +0100

    displaying the guessed letters

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -5,17 +5,32 @@
 word = "troubleshoot"
 wrong_guesses = 8
 guesses = 0
+guessed_letters = set()
+display_word = ["_" for _ in word]
 
 while guesses < wrong_guesses:
+    print("Word: ", display_word)
     user_guess = input("Guess a letter: ").lower()
     
-    if user_guess in word:
-        print("You guessed right!")
+    guessed_letters.add(user_guess)
 
+    # If the guess is right
+    if user_guess in word:
+        for i, char in enumerate(word):
+            if char == user_guess:
+                display_word[i] = user_guess
+        
+        # Check if player has guessed the full word
+        if "_" not in display_word:
+            print(f"Congratulations! You have guessed the word: {word}")
+            break
+    
+    # If the guess is wrong
     else:
         guesses += 1
-        print("Wrong guess!")
-        
+        print(f"Wrong guess! You have {wrong_guesses - guesses} guesses left.")
+
+    # Check if the player has reached maximum guesses  
     if guesses == wrong_guesses:
-        print("You have lost the game!")
+        print(f"You have lost the game! The word was: {word}")
 
